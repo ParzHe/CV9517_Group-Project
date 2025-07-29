c55df3f8f1fb93c21adf0a6bf9000656b642609f
Lintao HE <z5543618@ad.unsw.edu.au>
2025-07-29 13:03:39 +1000

    Move data download to data folder utils file

---
+++ data/utils.py
@@ -0,0 +1,37 @@
+import kagglehub
+import os
+
+def download_dataset(handle: str = "meteahishali/aerial-imagery-for-standing-dead-tree-segmentation") -> str:
+    """Download the dataset from Kaggle.
+    Args:
+        handle (str): The Kaggle dataset handle, e.g., "meteahishali/aerial-imagery-for-standing-dead-tree-segmentation".
+    Returns:
+        str: Path to the downloaded dataset files.
+    """
+    
+    home_path = os.path.expanduser("~")
+    kaggle_datasets_dir = os.path.join(home_path, ".cache", "kagglehub", "datasets")
+    dataset_folder_dir = os.path.join(kaggle_datasets_dir, handle.split("/")[0], handle.split("/")[1], "versions", "1", )
+
+    if os.path.exists(dataset_folder_dir):
+        path = dataset_folder_dir
+    else:
+        print(f"[white]Downloading dataset from Kaggle: {handle}...[/white]")
+        path = kagglehub.dataset_download(handle)
+        print(f"[green]Dataset successfully downloaded to {path}[/green]")
+    
+    path = os.path.join(path, "USA_segmentation")
+    
+    # List all folder names in the dataset directory
+    dataset_folders_expected = ["RGB_images", "NRG_images", "masks"]
+    folder_names = os.listdir(path)
+    
+    if not all(folder in folder_names for folder in dataset_folders_expected):
+        raise RuntimeError(f"Expected folders {dataset_folders_expected} not found in {path}. Check your dataset download.")
+
+    return path
+
+if __name__ == "__main__":
+    # Example usage
+    dataset_path = download_dataset()
+    print(f"Dataset downloaded to: {dataset_path}")
